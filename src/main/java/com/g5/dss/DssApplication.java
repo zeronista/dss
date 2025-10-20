d6cebc8806ca8cce72399226a1371736149634e6
Huy Nguyen <huynphe186731@fpt.edu.vn>
2025-10-20 09:43:07 +0700

    Initialize Spring Boot project with Maven wrapper, configuration files, and basic application structure. Added .gitignore, .gitattributes, and pom.xml for dependency management. Created main application class and test class for context loading.

---
+++ src/main/java/com/g5/dss/DssApplication.java
@@ -0,0 +1,13 @@
+package com.g5.dss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/g5/dss/DssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g5.dss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
